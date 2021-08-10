6af6b7fc5c20f36bbf155960a5d28e287dd417b6
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-08-11 00:56:46 +0200

    feat: language testing buttons

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,10 +1,10 @@
 import React, { useContext } from 'react';
-import { Button, StatusBar, Text } from 'react-native';
+import { Button, Text } from 'react-native';
 import { LocalizationContext } from '../../localization/LocalizationProvider';
 import Page from '../_common/Page/Page';
 
 const Home = ({ navigation }) => {
-  const { t } = useContext(LocalizationContext);
+  const { t, setLanguage } = useContext(LocalizationContext);
   return (
     <Page>
       <Text>{t('hello')}</Text>
@@ -12,6 +12,8 @@ const Home = ({ navigation }) => {
         title="Go to Login"
         onPress={() => navigation.navigate('login')}
       />
+      <Button title="Arabic" onPress={() => setLanguage('ar')} />
+      <Button title="English" onPress={() => setLanguage('en')} />
       <Text>{t('hello')}</Text>
     </Page>
   );

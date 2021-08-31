9a59863b5b4b87c9355415d8201e936976c5b8e6
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-08-31 23:46:18 +0200

    feat: bio text input

--- components/Register/OptionalInfo.js
+++ components/Register/OptionalInfo.js
@@ -8,12 +8,21 @@ import { LocalizationContext } from '../../localization/LocalizationProvider';
 const OptionalInfo = ({ navigation }) => {
   const { t } = useContext(LocalizationContext);
 
+  const [bio, setBio] = useState('');
+
   const onRegisterClick = () => {
     navigation.navigate('register/rollSelection');
   };
   return (
     <LoginBack>
-
+      <TextInput
+        text={bio}
+        setText={setBio}
+        title={t('Register/Bio')}
+        subtitle={t('TextInput/optional')}
+        multiline
+        style={styles.bioGap}
+      />
       <Text style={styles.addLater}>
         {t('Register/You can always add them later')}
       </Text>
@@ -29,5 +38,6 @@ OptionalInfo.defaultProps = {};
 export default OptionalInfo;
 
 const styles = StyleSheet.create({
+  bioGap: {marginBottom: 10},
   addLater: { width: '100%', marginBottom: 7 },
 });

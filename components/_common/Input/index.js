ad65bdf951e01536d1dd692d92e501d60793e6c5
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-09-13 20:44:31 +0200

    feat: TransparentTextInput

--- components/_common/Input/index.js
+++ components/_common/Input/index.js
@@ -1,3 +1,7 @@
 export { default as TextInput, TextInputFormik } from './TextInput';
+export {
+  default as TransparentTextInput,
+  TransparentTextInputFormik,
+} from './TransparentTextInput';
 export { default as TextInputGroup } from './TextInputGroup';
 export { default as AddProfileImage } from './AddProfileImage';
--- components/_common/Input/isTextInput.jsx
+++ components/_common/Input/isTextInput.jsx
@@ -1,5 +1,12 @@
 import TextInput, { TextInputFormik } from './TextInput';
+import TransparentTextInput, {
+  TransparentTextInputFormik,
+} from './TransparentTextInput';
 
-const isTextInput = (type) => type === TextInput || type === TextInputFormik;
+const isTextInput = (type) =>
+  type === TextInput ||
+  type === TextInputFormik ||
+  type === TransparentTextInput ||
+  type === TransparentTextInputFormik;
 
 export default isTextInput;

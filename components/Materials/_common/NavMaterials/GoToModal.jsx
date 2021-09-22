a43c8b8b1d9673d745954d7ca3e4e62ea32c56d8
DigitalPhoenixX <mohamed.xyz@hotmail.com>
2021-09-22 14:59:49 +0200

    feat: question number validation

--- components/Materials/_common/NavMaterials/GoToModal.jsx
+++ components/Materials/_common/NavMaterials/GoToModal.jsx
@@ -1,12 +1,16 @@
-import React, { useState } from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import { Dimensions, FlatList, StyleSheet, View } from 'react-native';
 import { Modal, Portal } from 'react-native-paper';
 import EduText from 'common/EduText';
-import { TransparentTextInput } from 'common/Input';
+import { TransparentTextInputFormik } from 'common/Input';
 import { TransparentButton } from 'common/Input/Button';
 import { Colors, Constants } from 'styles';
 import PressableText from 'common/PressableText';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { LocalizationContext } from 'localization';
+import { requiredError } from 'validation';
 
 const GoToModal = ({
   isVisible,
@@ -15,41 +19,78 @@ const GoToModal = ({
   currentIndex,
   maxIndex,
 }) => {
-  const [newIndexTextInput, setNewIndexTextInput] = useState('');
+  const { t } = useContext(LocalizationContext);
+
+  const formik = useFormik({
+    initialValues: {
+      newIndex: '',
+    },
+    onSubmit: ({ newIndex }) => {
+      changeIndexAndDismiss(parseInt(newIndex, 10) - 1);
+    },
+    validationSchema: yup.object().shape({
+      newIndex: yup
+        .number()
+        .typeError(t('TextInput/number'))
+        .min(1, t('TextInput/min', { min: 1 }))
+        .max(maxIndex, t('TextInput/max', { max: maxIndex }))
+        .integer(t('TextInput/integer'))
+        .required(requiredError(t)),
+    }),
+  });
+
+  const dismiss = () => {
+    setIsVisible(false);
+    formik.resetForm();
+  };
+  const changeIndexAndDismiss = (i) => {
+    changeIndex(i);
+    dismiss();
+  };
   return (
     <Portal>
-      <Modal visible={isVisible} onDismiss={() => setIsVisible(false)}>
+      <Modal visible={isVisible} onDismiss={dismiss}>
         <View style={styles.modalItemsContainer}>
-          <EduText style={styles.modalTitle}>Go to question</EduText>
+          <EduText style={styles.title}>
+            {t('MaterialNav/Go to question')}
+          </EduText>
           <FlatList
-            data={[...Array(maxIndex + 20).keys()]}
+            data={[...Array(maxIndex).keys()]}
+            keyboardShouldPersistTaps="handled"
             numColumns={5}
             keyExtractor={(item) => item}
             renderItem={({ item }) => (
               <PressableText
-                onPress={() => changeIndex(item)}
+                onPress={() => changeIndexAndDismiss(item)}
                 style={[
-                  styles.modalNumberContainer,
-                  currentIndex === item && styles.modalNumChosen,
+                  styles.numberText,
+                  currentIndex === item && styles.numberTextChosen,
                 ]}
+                pressableProps={{
+                  style: styles.numberContainer,
+                }}
               >
                 {item + 1}
               </PressableText>
             )}
+            style={styles.numberGrid}
           />
+          <EduText style={styles.errorText}>
+            {(formik.touched.newIndex && formik.errors.newIndex) || ''}
+          </EduText>
           <View style={styles.container}>
-            <TransparentTextInput
-              title="Enter question number"
-              placeholder="Enter question number"
-              setText={setNewIndexTextInput}
-              text={newIndexTextInput}
-              style={{ flex: 1 }}
+            <TransparentTextInputFormik
+              formik={formik}
+              formikKey="newIndex"
+              TextInputProps={{
+                placeholder: t('MaterialNav/Question Number'),
+                keyboardType: 'numeric',
+              }}
+              hideTitle
+              style={styles.newIndexTextInput}
             />
-            <EduText style>(1-{maxIndex})</EduText>
-            <TransparentButton
-              text="Go"
-              onPress={() => changeIndex(parseInt(newIndexTextInput, 10) - 1)}
-            />
+            <EduText style={styles.minMaxText}>(1-{maxIndex})</EduText>
+            <TransparentButton text="Go" onPress={formik.handleSubmit} />
           </View>
         </View>
       </Modal>
@@ -71,29 +112,47 @@ export default GoToModal;
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
-    alignItems: 'flex-end',
+    alignItems: 'center',
   },
+  minMaxText: { marginHorizontal: 10 },
   modalItemsContainer: {
     backgroundColor: Colors.background,
-    padding: Constants.commonMargin / 2,
+    padding: Constants.commonMargin,
     paddingTop: Constants.commonMargin,
     width: '90%',
     alignSelf: 'center',
     borderRadius: Constants.borderRadius,
   },
-  modalTitle: {
+  title: {
     fontSize: 22,
   },
-  modalNumberContainer: {
+  numberContainer: {
+    margin: Constants.commonMargin / 2,
+    borderRadius: Constants.borderRadius,
+  },
+  numberText: {
     backgroundColor: Colors.foreground,
     borderRadius: Constants.borderRadius,
     padding: Constants.commonMargin / 2,
-    margin: Constants.commonMargin / 2,
     width: Dimensions.get('window').width * 0.1,
     textAlign: 'center',
+    fontSize: 16,
   },
-  modalNumChosen: {
+  numberTextChosen: {
     color: Colors.white,
     backgroundColor: Colors.accent,
+  },
+  numberGrid: {
+    alignSelf: 'center',
+
+    maxHeight: 175,
+
+    marginBottom: 5,
+  },
+  newIndexTextInput: {
+    flex: 1,
+  },
+  errorText: {
+    color: Colors.error,
   },
 });

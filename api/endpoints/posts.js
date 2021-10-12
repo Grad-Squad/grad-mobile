5eaff99e42271783aa60add4544fc4de01f83207
Ramez <rameznoshy@gmail.com>
2021-10-12 22:32:55 +0200

    fix: typo postID => postId

--- api/endpoints/posts.js
+++ api/endpoints/posts.js
@@ -5,14 +5,15 @@ import endpoints from './endpoints';
 
 export const useAPIAddComment = (mutationConfig) => {
   const { axios } = useAxios();
-  return useMutation(async ({ postID, content }) => {
-    const {
-      data,
-    } = await axios.post(formatString(endpoints.posts.comments,postID), {
-        comment:{
-            content,
-        }
-    });
+  return useMutation(async ({ postId, content }) => {
+    const { data } = await axios.post(
+      formatString(endpoints.posts.comments, postId),
+      {
+        comment: {
+          content,
+        },
+      }
+    );
 
     return data;
   }, mutationConfig);

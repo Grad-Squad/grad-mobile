f2743a5f947e8bcec22cbcc48b7589498abf16f0
Ramez <rameznoshy@gmail.com>
2021-10-12 22:32:05 +0200

    feat: use refresh token to get new access tokens if 401 unauthorized

--- api/endpoints/auth.js
+++ api/endpoints/auth.js
@@ -3,15 +3,100 @@ import { useMutation } from 'react-query';
 import endpoints from './endpoints';
 
 export const useAPILogin = (mutationConfig) => {
-  const { axios } = useAxios();
-  return useMutation(async ({ email, password }) => {
+  const { axios, updateAccessToken, updateRefreshToken } = useAxios();
+
+  return useMutation(
+    async ({ email, password }) => {
+      const {
+        data: { data },
+      } = await axios.post(endpoints.auth.login, {
+        email,
+        password,
+      });
+
+      return data;
+    },
+    {
+      // ...mutationConfig,
+      onSuccess: (data, variables, context) => {
+        // todo el one line elli ta7t da sus
+        mutationConfig.onSuccess?.(data, variables, context);
+        updateAccessToken(data?.payload?.token);
+        updateRefreshToken(data?.payload?.refresh_token);
+      },
+    }
+  );
+};
+
+export const useAPIRefreshToken = (mutationConfig) => {
+  const { updateRefreshToken } = useAxios();
+  return useMutation(
+    async ({ refreshToken }) => {
+      const { data } = await axios.post(endpoints.auth.refresh, {
+        refreshToken,
+      });
+
+      return data?.data?.payload?.token;
+    },
+    {
+      ...mutationConfig,
+      onSuccess: (data, variables, context) => {
+        // todo el one line elli ta7t da sus
+        mutationConfig.onSuccess?.(data, variables, context);
+
+        updateRefreshToken(data?.data?.payload?.refresh_token);
+      },
+    }
+  );
+};
+
+export const useAPIRegister = (mutationConfig) => {
+  const { updateAccessToken, updateRefreshToken } = useAxios();
+  return useMutation(
+    async ({ email, password, name }) => {
+      const {
+        data: {
+          data: { user },
+        },
+      } = await axios.post(endpoints.auth.register, {
+        user: {
+          email,
+          password,
+        },
+        profile: {
+          name,
+        },
+      });
+
+      return user;
+    },
+    {
+      ...mutationConfig,
+      onSuccess: (data, variables, context) => {
+        // todo el one line elli ta7t da sus
+        mutationConfig.onSuccess?.(data, variables, context);
+
+        updateAccessToken(data?.data?.payload?.token);
+        updateRefreshToken(data?.data?.payload?.refresh_token);
+      },
+    }
+  );
+};
+
+export const useAPIUpdateAccount = (mutationConfig) => {
+  // const { axios } = useAxios();
+  return useMutation(async ({ userInfo, profileId }) => {
     const {
       data: {
         data: { user },
       },
-    } = await axios.post(endpoints.auth.login, {
-      email,
-      password,
+    } = await axios.post(endpoints.auth.register, {
+      user: {
+        ...userInfo,
+      },
+      profile: {
+        name,
+      },
     });
 
     return user;
--- api/endpoints/endpoints.js
+++ api/endpoints/endpoints.js
@@ -1,9 +1,11 @@
 export default Object.freeze({
   auth: {
     login: '/auth/login',
+    register: '/auth/register',
+    refresh: '/auth/refresh',
   },
   posts: {
-    post:'posts/{0}',
+    post: 'posts/{0}',
     comments: 'posts/{0}/comments',
   },
 });

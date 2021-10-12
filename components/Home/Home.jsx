bdc71ad817ee2843d6b27e6384244d9d61b7f976
Ramez <rameznoshy@gmail.com>
2021-10-12 22:32:31 +0200

    test: button in /home for testing api calls

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import { StyleSheet, Text } from 'react-native';
 import Page from 'common/Page/Page';
 import EduText from 'common/EduText';
+import { Button } from 'common/Input/Button';
 import Header from './Header';
+import { useAPIAddComment } from 'api/endpoints/posts';
 
 const Home = () => {
+  const mutation = useAPIAddComment();
   return (
     <Page>
       <Header />
+      <Button
+        text="click me"
+        onPress={() => {
+          mutation.mutate({ postId: 1, content: 'hi, ana comment' });
+        }}
+      />
+
       <EduText style={{ fontSize: 100 }}>Home</EduText>
     </Page>
   );

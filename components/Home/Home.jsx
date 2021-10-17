2e068562a241efb04101b81c97ffbce99164aae7
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-10-17 07:31:13 +0200

    feat: pull to refresh feed

--- components/Home/Home.jsx
+++ components/Home/Home.jsx
@@ -4,10 +4,11 @@ import Page from 'common/Page/Page';
 import { useAPIFeed } from 'api/endpoints/posts';
 import LoadingIndicator from 'common/LoadingIndicator';
 import Post from 'components/Post/Post';
+import QueryRefreshControl from 'common/QueryRefreshControl';
 import Header from './Header';
 
 const Home = () => {
-  const { data, isLoading } = useAPIFeed();
+  const { data, isLoading, refetch, isFetching } = useAPIFeed();
 
   const posts = data?.pages[0].data;
 
@@ -29,6 +30,13 @@ const Home = () => {
             />
           )}
           keyExtractor={(item) => item.id.toString()}
+          refreshControl={
+            <QueryRefreshControl
+              refetch={refetch}
+              isFetching={isFetching}
+              isLoading={isLoading}
+            />
+          }
         />
       )}
     </Page>
---
+++ components/_common/QueryRefreshControl.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { RefreshControl } from 'react-native';
+import { Colors } from 'styles';
+
+const QueryRefreshControl = ({ refetch, isFetching, isLoading, ...props }) => {
+  const [isRefetching, setIsRefetching] = useState(false);
+  useEffect(() => {
+    setIsRefetching(isFetching && !isLoading);
+  }, [isFetching, isLoading]);
+  return (
+    <RefreshControl
+      refreshing={isRefetching}
+      onRefresh={() => {
+        setIsRefetching(true);
+        refetch();
+      }}
+      tintColor={Colors.accent}
+      titleColor={Colors.accent}
+      colors={[Colors.accent]}
+      // eslint-disable-next-line react/jsx-props-no-spreading
+      {...props}
+    />
+  );
+};
+
+QueryRefreshControl.propTypes = {
+  refetch: PropTypes.func.isRequired,
+  isFetching: PropTypes.bool.isRequired,
+  isLoading: PropTypes.bool.isRequired,
+};
+QueryRefreshControl.defaultProps = {};
+
+export default QueryRefreshControl;

ecc54014b9bc49f8f88cb5766558440c6142e23b
Ramez <rameznoshy@gmail.com>
2021-10-19 01:00:16 +0200

    feat: post (card) context menu

--- components/Post/Options/Options.jsx
+++ components/Post/Options/Options.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { useState } from 'react';
+import { useRef } from 'react';
+import { Button } from 'react-native';
+import { Alert } from 'react-native';
 import { View, StyleSheet, TouchableOpacity } from 'react-native';
 
 import { Icon } from 'react-native-elements';
 import { HIT_SLOP_OBJECT } from '../../../constants';
+import ContextMenu from '../../_common/ContextMenu';
+import { IconNames } from '../../_common/Icon/Icon';
 
 const styles = StyleSheet.create({
   OptionsContainer: {
@@ -15,19 +21,55 @@ const styles = StyleSheet.create({
 });
 
 function Options() {
+  const anchorButton = useRef(null);
+  const [visible, setVisible] = useState(false);
   const optionHandler = () => {
-    console.log('OPTIONS');
+    setVisible((prev) => !prev);
   };
+
+  const items = [
+    {
+      titleKey: 'ContextMenu/Edit',
+      onPress: () => Alert.alert('WIP'),
+      iconName: IconNames.edit,
+    },
+    {
+      divider: true,
+      key: 'divider1',
+    },
+    {
+      titleKey: 'ContextMenu/Share',
+      onPress: () => Alert.alert('WIP'),
+      iconName: IconNames.share,
+    },
+    {
+      divider: true,
+      key: 'divider2',
+    },
+    {
+      titleKey: 'ContextMenu/Report',
+      onPress: () => Alert.alert('WIP'),
+      iconName: IconNames.report,
+    },
+  ];
 
   return (
     <View style={styles.OptionsContainer}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={optionHandler}
-        hitSlop={HIT_SLOP_OBJECT}
-      >
-        <Icon name="options" type="simple-line-icon" />
-      </TouchableOpacity>
+      <ContextMenu
+        visible={visible}
+        setVisible={setVisible}
+        anchor={
+          <TouchableOpacity
+            style={styles.button}
+            onPress={optionHandler}
+            hitSlop={HIT_SLOP_OBJECT}
+            ref={anchorButton}
+          >
+            <Icon name="options" type="simple-line-icon" />
+          </TouchableOpacity>
+        }
+        items={items}
+      ></ContextMenu>
     </View>
   );
 }

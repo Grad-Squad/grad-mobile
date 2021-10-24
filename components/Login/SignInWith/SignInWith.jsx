37cee6145ede95bfe5737679b24bf292121a98d1
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-10-24 13:37:50 +0200

    refactor: sign in with facebook

--- components/Login/SignInWith/SignInWith.jsx
+++ components/Login/SignInWith/SignInWith.jsx
@@ -4,29 +4,13 @@ import { useLocalization } from 'localization';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Styles } from 'styles';
-import * as FacebookSdk from 'expo-facebook';
 import * as GoogleSignIn from 'expo-google-sign-in';
-import { useAPIfacebookLogin } from 'api/endpoints/auth';
-import ScreenNames from 'navigation/ScreenNames';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { useNavigation } from '@react-navigation/core';
-import Facebook from './Facebook/Facebook';
+import SignInWithFacebook from './Facebook/SignInWithFacebook';
 import Google from './Google/Google';
 
 const SignInWith = ({ disabled }) => {
   const { t } = useLocalization();
   const [user, setUser] = useState({});
-
-  const navigation = useNavigation();
-
-  const facebookLoginMutation = useAPIfacebookLogin({
-    onSuccess: () => {
-      navigation.reset({
-        index: 0,
-        routes: [{ name: ScreenNames.HOME }],
-      });
-    },
-  });
 
   const signInWithGoogle = async () => {
     try {
@@ -52,19 +36,6 @@ const SignInWith = ({ disabled }) => {
     initAsync();
   }, []);
 
-  const loginWithFacebook = async () => {
-    await FacebookSdk.initializeAsync({
-      appId: '302939171663614',
-    });
-    const { type, token } = await FacebookSdk.logInWithReadPermissionsAsync({
-      permissions: ['public_profile', 'email'],
-      behavior: 'native',
-    });
-    if (type === 'success') {
-      facebookLoginMutation.mutate({ accessToken: token });
-    }
-  };
-
   return (
     <View style={[Styles.cardFooter, styles.background]}>
       <WhiteButton
@@ -75,14 +46,7 @@ const SignInWith = ({ disabled }) => {
         smallButton
         disabled={disabled}
       />
-      <WhiteButton
-        text={t('Login/Sign In With Facebook')}
-        onPress={loginWithFacebook}
-        leftIcon={<Facebook />}
-        smallButton
-        disabled={disabled}
-        loading={facebookLoginMutation.isLoading}
-      />
+      <SignInWithFacebook disabled={disabled} />
     </View>
   );
 };

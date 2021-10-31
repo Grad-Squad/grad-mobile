33b7cf63339347486f05d3b6132f9e51f58d960f
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-10-31 10:47:44 +0200

    fix: api add comment missing api url

--- api/endpoints/posts.js
+++ api/endpoints/posts.js
@@ -5,12 +5,15 @@ import endpoints from './endpoints';
 
 export const useAPIAddComment = (mutationConfig) => {
   const { axios } = useAxios();
-  return useMutation(async ({ postId, content }) => {
-    const { data } = await axios.post({
-      comment: {
-        content,
-      },
-    });
+  return useMutation(async ({ postID, content }) => {
+    const { data } = await axios.post(
+      formatString(endpoints.posts.comments, postID),
+      {
+        comment: {
+          content,
+        },
+      }
+    );
 
     return data;
   }, mutationConfig);

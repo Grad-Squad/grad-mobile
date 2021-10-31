e6d3532eae42edcb9b575356bae757855dd7c560
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2021-10-31 10:51:22 +0200

    feat: go back button

---
+++ components/_common/GoBackButton.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Pressable, StyleSheet } from 'react-native';
+import { Colors, Constants } from 'styles';
+import { Icon } from './Icon';
+import { IconNames } from './Icon/Icon';
+import pressableAndroidRipple from './pressableAndroidRipple';
+
+const GoBackButton = ({ onPress }) => (
+  <Pressable
+    style={styles.backIconContainer}
+    onPress={onPress}
+    android_ripple={pressableAndroidRipple}
+  >
+    <Icon name={IconNames.arrowLeft} size={40} color={Colors.black} />
+  </Pressable>
+);
+
+GoBackButton.propTypes = { onPress: PropTypes.func.isRequired };
+GoBackButton.defaultProps = {};
+
+export default GoBackButton;
+
+const borderWidth = 1;
+const styles = StyleSheet.create({
+  backIconContainer: {
+    position: 'absolute',
+    top: -borderWidth,
+    left: '6.5%',
+
+    paddingTop: 1 * Constants.fromScreenStartPadding + 7,
+    padding: 5,
+
+    borderWidth,
+    borderColor: Colors.black,
+    borderBottomLeftRadius: 7,
+    borderBottomRightRadius: 7,
+  },
+});

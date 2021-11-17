a4d1e53e5418caf13802f2a2cc13f2fdb1896e60
Ramez <rameznoshy@gmail.com>
2021-11-17 02:52:46 +0200

    feat: profile header

--- components/Profile/Profile.jsx
+++ components/Profile/Profile.jsx
@@ -3,13 +3,30 @@ import { StyleSheet } from 'react-native';
 import Page from 'common/Page/Page';
 import { navigationPropType, routeParamPropType } from 'proptypes';
 import EduText from 'common/EduText';
+import PropTypes from 'prop-types';
 import ProfileHeader from './ProfileHeader';
 
 const Profile = ({ navigation, route }) => {
   const { profileId } = route.params;
+  const profile = {
+    id: 2,
+    createdAt: '2021-09-26T17:49:56.650Z',
+    updatedAt: '2021-11-14T14:50:32.584Z',
+    name: 'Sameh Initial',
+    role: 'student',
+    profilePicture: '',
+    // biography:
+    // 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    // biography: 'Lorem ipsum dolor sit ',
+    biography: '',
+    numPosts: 100,
+    numFollowers: 150,
+    isFollowed: true,
+  };
+
   return (
     <Page>
-      <ProfileHeader navigation={navigation} />
+      <ProfileHeader navigation={navigation} profile={profile} />
       <EduText>{profileId}</EduText>
       <EduText>{profileId}</EduText>
       <EduText>{profileId}</EduText>
@@ -26,7 +43,11 @@ const Profile = ({ navigation, route }) => {
 
 Profile.propTypes = {
   navigation: navigationPropType.isRequired,
-  route: routeParamPropType.isRequired,
+  route: routeParamPropType(
+    PropTypes.exact({
+      profileId: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 Profile.defaultProps = {};
 
--- components/Profile/ProfileHeader.jsx
+++ components/Profile/ProfileHeader.jsx
@@ -1,23 +1,171 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, Image, StyleSheet, View } from 'react-native';
 import { navigationPropType } from 'proptypes';
+import PropTypes from 'prop-types';
 import GoBackButton from 'common/GoBackButton';
+import EduText from 'common/EduText';
+import { MainActionButton, SecondaryActionButton } from 'common/Input/Button';
+import { Constants, Fonts } from 'styles';
+import { useLocalization } from 'localization';
+import { Icon } from 'common/Icon';
 
-const ProfileHeader = ({ navigation }) => (
-  <View>
-    <GoBackButton
-      onPress={() => {
-        navigation.goBack();
-      }}
-    />
+const NumBox = ({ title, number }) => (
+  <View style={styles.numBox}>
+    <EduText style={styles.numBoxTitle}>{title}</EduText>
+    <EduText style={styles.numBoxNumber}>{number}</EduText>
   </View>
 );
 
+NumBox.propTypes = {
+  title: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+};
+
+const ProfileHeader = ({ navigation, profile }) => {
+  const { t } = useLocalization();
+  const [isFollowed, setIsFollowed] = useState(profile.isFollowed);
+  const followButton = isFollowed ? (
+    <SecondaryActionButton
+      onPress={() => setIsFollowed(false)}
+      text={t('Profile/Unfollow')}
+      style={styles.followBtn}
+    />
+  ) : (
+    <MainActionButton
+      onPress={() => setIsFollowed(true)}
+      text={t('Profile/Follow')}
+      style={styles.followBtn}
+    />
+  );
+  return (
+    <View>
+      <GoBackButton
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+      <View style={[styles.row, styles.container]}>
+        <View
+          style={[
+            styles.leftHeader,
+            !profile.biography && styles.leftHeaderCenter,
+          ]}
+        >
+          <View style={[styles.row, styles.nameContainer]}>
+            <View>
+              <EduText numberOfLines={1} style={styles.name}>
+                {profile.name}
+              </EduText>
+              <EduText style={styles.role}>{profile.role}</EduText>
+            </View>
+          </View>
+          <EduText
+            numberOfLines={4}
+            style={styles.biography}
+            placeholder="biography..."
+          >
+            {profile.biography && profile.biography}
+          </EduText>
+          <View style={[styles.row, styles.followBtnContainer]}>
+            {followButton}
+            <Icon name="dots-vertical" />
+          </View>
+        </View>
+        <View style={styles.rightHeader}>
+          <Image
+            style={styles.profileImage}
+            source={{
+              uri: 'https://cdn.discordapp.com/attachments/810207976232976446/873648416113192980/unknown.png',
+            }}
+          />
+          <NumBox title="Followers" number={123} />
+          <NumBox title="Posts" number={1123} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
 ProfileHeader.propTypes = {
   navigation: navigationPropType.isRequired,
+  profile: PropTypes.exact({
+    id: PropTypes.number.isRequired,
+    createdAt: PropTypes.string.isRequired,
+    updatedAt: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    role: PropTypes.string.isRequired,
+    profilePicture: PropTypes.string.isRequired,
+    biography: PropTypes.string.isRequired,
+    numFollowers: PropTypes.number.isRequired,
+    numPosts: PropTypes.number.isRequired,
+    isFollowed: PropTypes.bool.isRequired,
+  }).isRequired,
 };
 ProfileHeader.defaultProps = {};
 
 export default ProfileHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  container: {
+    margin: Constants.commonMargin,
+  },
+  leftHeader: {
+    flexGrow: 3,
+    flexShrink: 1,
+    justifyContent: 'space-between',
+  },
+  leftHeaderCenter: {
+    justifyContent: 'center',
+  },
+  rightHeader: {
+    flexGrow: 1,
+    flexShrink: 0,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  numBox: {
+    marginTop: Constants.commonMargin,
+    alignItems: 'center',
+  },
+  numBoxTitle: {
+    fontFamily: Fonts.light,
+    fontSize: 18,
+  },
+  numBoxNumber: {
+    fontSize: 20,
+  },
+  biography: {
+    marginTop: Constants.commonMargin / 2,
+    marginRight: Constants.commonMargin / 2,
+  },
+  followBtnContainer: {
+    marginTop: Constants.commonMargin,
+    alignItems: 'center',
+  },
+  nameContainer: {
+    marginLeft: Dimensions.get('window').width * 0.18,
+    marginTop: Dimensions.get('window').width * 0.05,
+  },
+
+  role: {
+    fontFamily: Fonts.light,
+    fontSize: 18,
+  },
+  name: {
+    fontSize: 24,
+  },
+
+  profileImage: {
+    marginTop: Constants.commonMargin,
+    borderRadius: 70,
+    width: 70,
+    height: 70,
+  },
+  followBtn: {
+    flex: 1,
+    marginRight: 7,
+  },
+});

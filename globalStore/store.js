5041ec9eb0eae3087d91caa3c008d001c446970c
Ramez <rameznoshy@gmail.com>
2021-12-06 20:07:25 +0200

    hotfix: missing redux reducers

--- globalStore/store.js
+++ globalStore/store.js
@@ -1,10 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createPost from './createPostSlice';
 import material from './materialNavSlice';
+import imageUpload from './imageUploadSlice';
+import profile from './profileSlice';
 
 export default configureStore({
   reducer: {
     createPost,
     material,
+    imageUpload,
+    profile,
   },
 });

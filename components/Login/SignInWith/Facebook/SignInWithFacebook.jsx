93cd17019d9199358ced80f3ebb1ced869055450
Ramez <rameznoshy@gmail.com>
2021-12-18 10:56:15 +0200

    fix: login w/ fb, w/ google did not set profile id

--- components/Login/SignInWith/Facebook/SignInWithFacebook.jsx
+++ components/Login/SignInWith/Facebook/SignInWithFacebook.jsx
@@ -7,16 +7,24 @@ import * as FacebookSdk from 'expo-facebook';
 import { useNavigation } from '@react-navigation/core';
 import { WhiteButton } from 'common/Input/Button';
 import { useLocalization } from 'localization';
+import { useStore } from 'globalStore/GlobalStore';
+import ReducerActions from 'globalStore/ReducerActions';
 import Facebook from './Facebook';
 
 const SignInWithFacebook = ({ disabled }) => {
   const { t } = useLocalization();
   const navigation = useNavigation();
+  const [, dispatch] = useStore();
+
   const facebookLoginMutation = useAPIfacebookLogin({
-    onSuccess: () => {
+    onSuccess: (data) => {
       navigation.reset({
         index: 0,
         routes: [{ name: ScreenNames.HOME }],
+      });
+      dispatch({
+        type: ReducerActions.setProfileId,
+        payload: data.user.profile.id,
       });
     },
   });

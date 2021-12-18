94e83948180ba865533cdb6739755f455547f6a6
Ramez <rameznoshy@gmail.com>
2021-12-18 06:41:11 +0200

    feat: materialItem with checkbox

--- components/_common/Input/Checkbox.jsx
+++ components/_common/Input/Checkbox.jsx
@@ -5,10 +5,17 @@ import { PressableIcon } from 'common/Icon';
 import { stylePropType } from 'proptypes';
 import { IconNames } from 'common/Icon/Icon';
 
-const Checkbox = ({ checked, setChecked, style, pressableProps, disabled }) => (
+const Checkbox = ({
+  checked,
+  setChecked,
+  style,
+  pressableProps,
+  disabled,
+  onPress,
+}) => (
   <PressableIcon
     android_ripple={pressableAndroidRipple}
-    onPress={() => setChecked(!checked)}
+    onPress={onPress || (() => setChecked(!checked))}
     style={style}
     name={checked ? IconNames.checkbox : IconNames.square}
     // eslint-disable-next-line react/jsx-props-no-spreading
@@ -19,12 +26,19 @@ const Checkbox = ({ checked, setChecked, style, pressableProps, disabled }) => (
 
 Checkbox.propTypes = {
   checked: PropTypes.bool.isRequired,
-  setChecked: PropTypes.func.isRequired,
+  setChecked: PropTypes.func,
+  onPress: PropTypes.func,
   style: stylePropType,
   disabled: PropTypes.bool,
   // eslint-disable-next-line react/forbid-prop-types
   pressableProps: PropTypes.object,
 };
-Checkbox.defaultProps = { style: {}, pressableProps: {}, disabled: false };
+Checkbox.defaultProps = {
+  style: {},
+  pressableProps: {},
+  disabled: false,
+  setChecked: () => {},
+  onPress: () => {},
+};
 
 export default React.memo(Checkbox);

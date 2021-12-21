44ed48935907e71bc2f83a5d4d2a6c609f639d30
Strykar1 <ahmedms.ftw@gmail.com>
2021-12-21 10:10:30 +0200

    feat: basic sentry setup

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import AppLoading from 'expo-app-loading';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import * as Sentry from 'sentry-expo';
 
 import GlobalStore from 'globalStore/GlobalStore';
 import { Colors } from 'styles';
@@ -31,12 +32,19 @@ const theme = {
   },
 };
 
-export default function App() {
+function App() {
   const ready = initStyles();
 
   if (!ready) {
     return <AppLoading />;
   }
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    enableInExpoDevelopment: true,
+    enableNative: false,
+    debug: true, // TODO set to false in production
+  });
 
   return (
     <LocalizationProvider>
@@ -59,3 +67,5 @@ export default function App() {
     </LocalizationProvider>
   );
 }
+
+export default App;
--- components/Dev/Dev.jsx
+++ components/Dev/Dev.jsx
@@ -8,6 +8,7 @@ import Page from 'common/Page/Page';
 import { Constants } from 'styles';
 import { useErrorSnackbar } from 'common/ErrorSnackbar/ErrorSnackbarProvider';
 import ScreenNames from 'navigation/ScreenNames';
+import * as Sentry from 'sentry-expo';
 
 LogBox.ignoreLogs(['Setting a timer']);
 
@@ -64,6 +65,10 @@ const Dev = ({ navigation }) => {
       <Button
         title="Go to images"
         onPress={() => navigation.navigate(ScreenNames.VIEW_IMAGES)}
+      />
+      <Button
+        title="Sentry crash"
+        onPress={() => {throw new Error("My first Sentry error!");}}
       />
       <Button title="Arabic" onPress={() => setLanguage('ar')} />
       <Button title="English" onPress={() => setLanguage('en')} />

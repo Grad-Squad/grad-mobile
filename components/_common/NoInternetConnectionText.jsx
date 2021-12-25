ffc603ee84aec704fc4cb6bf8cb9b90936e0b975
Ramez <rameznoshy@gmail.com>
2021-12-25 17:01:53 +0200

    localize: no internet connection text

--- components/_common/NoInternetConnectionText.jsx
+++ components/_common/NoInternetConnectionText.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { useNetInfo } from '@react-native-community/netinfo';
 import { Colors } from 'styles';
+import { useLocalization } from 'localization';
 import EduText from './EduText';
 
 const NoInternetConnectionText = () => {
   const netInfo = useNetInfo();
+  const { t } = useLocalization();
   return netInfo.isConnected ? null : (
-    <EduText style={styles.text}>No Internet Connection</EduText>
+    <EduText style={styles.text}>{t('General/No Internet Connection')}</EduText>
   );
 };
 

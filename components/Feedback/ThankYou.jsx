2dd6f3e3b1a17e8ce69b74d1833244461db3bb1a
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2022-01-18 17:17:04 +0200

    feat: feedback thankyou new image style

--- components/Feedback/ThankYou.jsx
+++ components/Feedback/ThankYou.jsx
@@ -4,7 +4,7 @@ import { Modal, StyleSheet, View } from 'react-native';
 import EduText from 'common/EduText';
 import { useLocalization } from 'localization';
 import { SecondaryActionButton } from 'common/Input/Button';
-import { Colors, Constants } from 'styles';
+import { Colors, Constants, Styles } from 'styles';
 import ResponsiveImage from 'common/ResponsiveImage';
 
 const ThankYou = ({ modalVisible, setModalVisible }) => {
@@ -21,11 +21,16 @@ const ThankYou = ({ modalVisible, setModalVisible }) => {
     >
       <View style={styles.centeredView}>
         <View style={styles.backgroundView}>
-          <ResponsiveImage
-            imageURI="https://c.tenor.com/_UaFpyE0SPYAAAAd/praying-cat.gif"
-            canMaximize={false}
-            maxWidthRatio={0.82}
-          />
+          <View style={styles.topImageContainer}>
+            <View style={styles.topImageRoundedCorner}>
+              <ResponsiveImage
+                imageURI="https://c.tenor.com/_UaFpyE0SPYAAAAd/praying-cat.gif"
+                canMaximize={false}
+                maxWidthRatio={0.82}
+                style={styles.topImage}
+              />
+            </View>
+          </View>
           <EduText style={styles.thankYou}>
             {t('Feedback/ThankYou/THANK YOU !!')}
           </EduText>
@@ -65,6 +70,20 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.background,
     width: '90%',
     padding: 15,
+  },
+  topImageContainer: {
+    alignItems: 'center',
+    width: '100%',
+    top: '-50%',
+    marginBottom: -150,
+  },
+  topImageRoundedCorner: {
+    borderRadius: 20,
+    overflow: 'hidden',
+    ...Styles.dropShadow,
+  },
+  topImage: {
+    marginBottom: 0,
   },
   thankYou: {
     fontFamily: 'Poppins_400Regular',

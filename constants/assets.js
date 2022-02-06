6d2db5dfff45197ce4b9053c0d20e75867a59f34
DigitalPhoenixX <MohamedSaidSallam@outlook.com>
2022-02-06 19:54:50 +0200

    fix: default profile image

--- constants/assets.js
+++ constants/assets.js
@@ -1,4 +1,4 @@
 export default {
   // eslint-disable-next-line global-require
-  images: { defaultProfile: require('assets/images/defaultUser.png') },
+  images: { defaultProfile: require('../assets/images/defaultUser.png') },
 };
